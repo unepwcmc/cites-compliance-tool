1fdf3c7ecbd9ceb5a92e6724275b4e64bc69a5b2
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-03 16:40:06 +0100

    populate dashboard controller with total count for each compliance type

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
+
   def index
+    @appendix_i_total_cnt = ShipmentsApiRetriever.api_call('appendix_i')['meta']['total']
+    @trade_suspensions_total_cnt = ShipmentsApiRetriever.api_call('trade_suspensions')['meta']['total']
+    @mandatory_quotas_total_cnt = ShipmentsApiRetriever.api_call('mandatory_quotas')['meta']['total']
   end
 end
