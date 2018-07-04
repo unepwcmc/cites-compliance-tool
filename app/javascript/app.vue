320a59cb224f7157be97c0e4429209e3a04e4e76
Robin Hawkes <rob.hawkes@gmail.com>
2018-07-04 10:38:01 +0100

    Initial dashboard skeleton

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,79 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="#">
+          UN Environment WCMC
+        </a>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="#">
+            Dashboard
+          </a>
+          <a class="navbar-item" href="#">
+            Search
+          </a>
+        </div>
+
+        <div class="navbar-end">
+          <a class="navbar-item" href="#">
+            Download All
+          </a>
+          <a class="navbar-item" href="#">
+            Name
+          </a>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container is-fluid is-marginless tile-container">
+      <div class="tile is-ancestor">
+        <div class="tile is-4 is-parent">
+        </div>
+        <div class="tile is-8 is-parent">
+        </div>
+      </div>
+
+      <div class="tile is-ancestor">
+        <div class="tile is-12 is-parent">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  components: {},
   data: function () {
-    return {
-      message: "Hello Vue!"
-    }
+    return {}
   }
 }
 </script>
 
+<style>
+html {
+  background: #f3f3f3;
+}
+
+/* Stops level elements being too wide */
+.level-item {
+  flex-shrink: 1;
+}
+</style>
+
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+.navbar {
+  background: #00a0d1;
+}
+
+.navbar-item, .navbar-link {
+  color: #fff;
+}
+
+.tile-container {
+  padding: 20px;
 }
 </style>
