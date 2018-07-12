a629c6f278311a8ad528265d932221535bc2f364
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-12 17:39:13 +0100

    add per page and timeout parameters to api call

--- lib/modules/shipments_api_retriever.rb
+++ lib/modules/shipments_api_retriever.rb
@@ -1,14 +1,13 @@
 module  ShipmentsApiRetriever
-
-  def self.api_call(compliance_type=nil)
+  def self.api_call(compliance_type = nil)
     response = HTTParty.get(ENV['SPECIES_API_URL'],
-                             headers: header,
-                             query: {compliance_type: compliance_type})
+                            headers: header,
+                            query: { compliance_type: compliance_type, per_page: 110_000 },
+                            read_timeout: 120)
     JSON.parse response.body
   end
 
   def self.header
-    {'X-Authentication-Token' => Rails.application.secrets["compliance_tool_token"]}
+    { 'X-Authentication-Token' => Rails.application.secrets['compliance_tool_token'] }
   end
-
 end
