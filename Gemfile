95d26714dc124799701b91fd0464b98ec2de0ef9
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-13 13:21:19 +0100

    refactor csv generator module to add compliance type column

--- Gemfile
+++ Gemfile
@@ -67,9 +67,6 @@ group :development do
   gem 'capistrano-nvm'
 end
 
-gem 'dotenv-rails'
-
-
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15', '< 4.0'
--- lib/modules/csv_downloader.rb
+++ lib/modules/csv_downloader.rb
@@ -1,23 +1,21 @@
 module CsvDownloader
-  def self.csv_generator
-    attributes = data_fetcher.first.keys
-    CSV.generate(headers: true) do |csv|
-      csv << attributes
-      data_fetcher.each do |data|
-        csv << data.values
-      end
-    end
-  end
+  HEADER = %w[ id year appendix taxon class order family genus
+               term importer_reported_quantity exporter_reported_quantity
+               unit importer exporter origin purpose source
+               compliance_type_taxonomic_rank
+               import_permit
+               export_permit origin_permit compliance_type ].freeze
 
-  def self.data_fetcher(compliance_type = nil)
-    data = []
-    if compliance_type.present?
-      data = ShipmentsApiRetriever.api_call(compliance_type)['shipments']
-    else
+  def self.csv_generator
+    CSV.generate(headers: true) do |csv|
+      csv << HEADER
       %w[trade_suspensions appendix_i mandatory_quotas].each do |type|
-        data << ShipmentsApiRetriever.api_call(type)['shipments']
+        data = ShipmentsApiRetriever.api_call(type)['shipments']
+        data.each do |row|
+          row[:compliance_type] = type
+          csv << row.values
+        end
       end
     end
-    data.flatten
   end
 end
