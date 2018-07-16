0f57e53f3ad2439caa01450a655174a5728ea9ab
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-07-16 17:43:16 +0100

    Use secrets

--- lib/modules/shipments_api_retriever.rb
+++ lib/modules/shipments_api_retriever.rb
@@ -1,6 +1,6 @@
 module  ShipmentsApiRetriever
   def self.api_call(compliance_type = nil, page = 1)
-    response = HTTParty.get(ENV['SPECIES_API_URL'],
+    response = HTTParty.get(Rails.application.secrets['species_api_url'],
                             headers: header,
                             query: { compliance_type: compliance_type,
                                      time_range_start: 2012, time_range_end: 2016,
