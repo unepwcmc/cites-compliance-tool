cd5340de008744ae24bc1eaeea7a2c52dcd7e973
Stacy Talbot <stacymtalbot@gmail.com>
2018-07-16 15:56:00 +0100

    Add static species data for the top species list

--- app/javascript/data/species.js
+++ app/javascript/data/species.js
@@ -1,88 +1,164 @@
 const data = {
-  '2014': [{
-    name: 'Species 1',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 2',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 3',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 4',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 5',
-    transactions: 123,
-    appendix: 'II'
-  }],
-  '2015': [{
-    name: 'Species 1',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 2',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 3',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 4',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 5',
-    transactions: 123,
-    appendix: 'II'
-  }],
-  '2016': [{
-    name: 'Species 1',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 2',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 3',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 4',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 5',
-    transactions: 123,
-    appendix: 'II'
-  }],
-  '2017': [{
-    name: 'Species 1',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 2',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 3',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 4',
-    transactions: 123,
-    appendix: 'II'
-  }, {
-    name: 'Species 5',
-    transactions: 123,
-    appendix: 'II'
-  }]
+  "2012": [
+    {
+      "name": "Loxodonta africana",
+      "appendix": "II",
+      "transactions": "4491",
+      "percent": "34.6367422489588154"
+    }, 
+    {
+      "name": "Troides rhadamantus",
+      "appendix": "II",
+      "transactions": "829",
+      "percent": "6.3936449174764769"
+    }, 
+    {
+      "name": "Hippopotamus amphibius",
+      "appendix": "II",
+      "transactions": "327",
+      "percent": "2.5219805645534475"
+    }, 
+    {
+      "name": "Monodon monoceros",
+      "appendix": "II",
+      "transactions": "300",
+      "percent": "2.3137436372049977"
+    }, 
+    {
+      "name": "Orchidaceae",
+      "appendix": "II",
+      "transactions": "297",
+      "percent": "2.2906062008329477"
+    }
+  ],
+  "2013": [
+    {
+      "name": "Loxodonta africana",
+      "appendix": "II",
+      "transactions": "3851",
+      "percent": "32.3994615514050143"
+    }, 
+    {
+      "name": "Troides rhadamantus",
+      "appendix": "II",
+      "transactions": "691",
+      "percent": "5.8135621739862023"
+    }, 
+    {
+      "name": "Falco cherrug",
+      "appendix": "II",
+      "transactions": "581",
+      "percent": "4.8881036513545347"
+    }, 
+    {
+      "name": "Strombus gigas",
+      "appendix": "II",
+      "transactions": "378",
+      "percent": "3.1802120141342756"
+    }, 
+    {
+      "name": "Physeter macrocephalus",
+      "appendix": "I",
+      "transactions": "218",
+      "percent": "1.8340905266700320"
+    }
+  ],
+  "2014": [
+    {
+      "name": "Loxodonta africana",
+      "appendix": "II",
+      "transactions": "6419",
+      "percent": "36.9120184013801035"
+    }, 
+    {
+      "name": "Caiman crocodilus fuscus",
+      "appendix": "II",
+      "transactions": "2032",
+      "percent": "11.6848763657274296"
+    }, 
+    {
+      "name": "Troides rhadamantus",
+      "appendix": "II",
+      "transactions": "622",
+      "percent": "3.5767682576193214"
+    }, 
+    {
+      "name": "Strombus gigas",
+      "appendix": "II",
+      "transactions": "514",
+      "percent": "2.9557216791259344"
+    }, 
+    {
+      "name": "Monodon monoceros",
+      "appendix": "II",
+      "transactions": "396",
+      "percent": "2.2771707878090857"
+    }
+  ],
+  "2015": [
+    {
+      "name": "Caiman crocodilus fuscus",
+      "appendix": "II",
+      "transactions": "1713",
+      "percent": "14.2382179369960934"
+    }, 
+    {
+      "name": "Loxodonta africana",
+      "appendix": "II",
+      "transactions": "1682",
+      "percent": "13.9805502451999003"
+    }, 
+    {
+      "name": "Troides rhadamantus",
+      "appendix": "II",
+      "transactions": "719",
+      "percent": "5.9762280774665448"
+    }, 
+    {
+      "name": "Monodon monoceros",
+      "appendix": "II",
+      "transactions": "653",
+      "percent": "5.4276452497714238"
+    }, 
+    {
+      "name": "Strombus gigas",
+      "appendix": "II",
+      "transactions": "546",
+      "percent": "4.5382761200232732"
+    }
+  ],
+  "2016": [
+    {
+      "name": "Loxodonta africana",
+      "appendix": "II",
+      "transactions": "2779",
+      "percent": "21.4230650632130743"
+    }, 
+    {
+      "name": "Caiman crocodilus fuscus",
+      "appendix": "II",
+      "transactions": "1261",
+      "percent": "9.7209374036386062"
+    }, 
+    {
+      "name": "Dalbergia cochinchinensis",
+      "appendix": "II",
+      "transactions": "1074",
+      "percent": "8.2793709528214616"
+    }, 
+    {
+      "name": "Pterocarpus erinaceus",
+      "appendix": "III",
+      "transactions": "1003",
+      "percent": "7.7320382362010484"
+    }, 
+    {
+      "name": "Troides rhadamantus",
+      "appendix": "II",
+      "transactions": "679",
+      "percent": "5.2343509096515572"
+    }
+  ]
 }
 
 module.exports = data
