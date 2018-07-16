8ee2ad899420a3152c6bdf5976d9bff72a0d0637
Stacy Talbot <stacymtalbot@gmail.com>
2018-07-16 16:25:41 +0100

    Add static data for the top chart

--- app/javascript/data/reported.js
+++ app/javascript/data/reported.js
@@ -1,19 +1,27 @@
 const data = {
+  '2012': {
+    year: 2012,
+    value: 12966
+  },
+  '2013': {
+    year: 2013,
+    value: 11886
+  },
   '2014': {
     year: 2014,
-    value: 23286
+    value: 17390
   },
   '2015': {
     year: 2015,
-    value: 23286
+    value: 12031
   },
   '2016': {
     year: 2016,
-    value: 23286
+    value: 12972
   },
   '2017': {
     year: 2017,
-    value: 23286
+    value: 336
   }
 }
 
