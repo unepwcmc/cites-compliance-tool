427aa35bd668807c71305cc18d4a1094642c7649
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-16 16:42:29 +0100

    add importer and exporter iso columns to csv

--- lib/modules/csv_downloader.rb
+++ lib/modules/csv_downloader.rb
@@ -1,8 +1,9 @@
 module CsvDownloader
   HEADER = %w[ id year appendix taxon class order family genus
                term importer_reported_quantity exporter_reported_quantity
-               unit importer exporter origin purpose source import_permit
-               export_permit origin_permit issue_type ].freeze
+               unit importer importer_iso exporter exporter_iso origin
+               purpose source import_permit export_permit origin_permit
+               issue_type ].freeze
 
   def self.csv_generator
     CSV.generate(headers: true) do |csv|
