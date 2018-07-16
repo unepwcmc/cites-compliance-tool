aa56d8b8df5f890b83588a9b1d7b006c711494d9
Stacy Talbot <stacymtalbot@gmail.com>
2018-07-16 16:15:25 +0100

    Add static data for issues by category chart

--- app/javascript/data/categories.js
+++ app/javascript/data/categories.js
@@ -1,44 +1,109 @@
 const data = {
-  '2014': [{
-    name: "Trade Suspensions",
-    value: 7356
-  }, {
-    name: "Appendix I Trade",
-    value: 17231
-  }, {
-    name: "Quotas",
-    value: 10898
-  }],
-  '2015': [{
-    name: "Trade Suspensions",
-    value: 7356
-  }, {
-    name: "Appendix I Trade",
-    value: 17231
-  }, {
-    name: "Quotas",
-    value: 10898
-  }],
-  '2016': [{
-    name: "Trade Suspensions",
-    value: 7356
-  }, {
-    name: "Appendix I Trade",
-    value: 17231
-  }, {
-    name: "Quotas",
-    value: 10898
-  }],
-  '2017': [{
-    name: "Trade Suspensions",
-    value: 7356
-  }, {
-    name: "Appendix I Trade",
-    value: 17231
-  }, {
-    name: "Quotas",
-    value: 10898
-  }]
+  "2014": [
+    {
+      "name": "Suspension",
+      "value": 17245, 
+      "percent": 99.1661874640598045
+    },
+    {
+      "name": "AppendixI", 
+      "value": 110,
+      "percent": 0.63254744105807935595
+    },
+    {
+      "name": "Quota", 
+      "value": 35,
+      "percent": 0.20126509488211615871
+    }
+  ],
+  "2017": [
+    {
+      "name": "Suspension",
+      "value": 333, 
+      "percent": 99.1071428571428571
+    },
+    {
+      "name": "AppendixI", 
+      "value": 3,
+      "percent": 0.89285714285714285714
+    },
+    {
+      "name": "Quota", 
+      "value": 0,
+      "percent": 0
+    }
+  ],
+  "2015": [
+    {
+      "name": "Suspension",
+      "value": 11918, 
+      "percent": 99.0607597040977475
+    },
+    {
+      "name": "AppendixI", 
+      "value": 103,
+      "percent": 0.85612168564541600864
+    },
+    {
+      "name": "Quota", 
+      "value": 10,
+      "percent": 0.08311861025683650569
+    }
+  ],
+  "2013": [
+    {
+      "name": "Suspension",
+      "value": 11758, 
+      "percent": 98.9231028100286051
+    },
+    {
+      "name": "AppendixI", 
+      "value": 117,
+      "percent": 0.98435133770822816759
+    },
+    {
+      "name": "Quota", 
+      "value": 11,
+      "percent": 0.09254585226316675080
+    }
+  ],
+  "2012": [
+    {
+      "name": "Suspension",
+      "value": 12796, 
+      "percent": 98.6888786055838346
+    },
+    {
+      "name": "AppendixI", 
+      "value": 138,
+      "percent": 1.06432207311429893568
+    },
+    {
+      "name": "Quota", 
+      "value": 32,
+      "percent": 0.24679932130186641987
+    },
+  ],
+  "2016": [
+    {
+      "name": "Suspension",
+      "value": 12670, 
+      "percent": 97.6719087264878199
+    },
+    {
+      "name": "AppendixI", 
+      "value": 292,
+      "percent": 2.2510021584952205
+    },
+    {
+      "name": "Quota", 
+      "value": 10,
+      "percent": 0.07708911501695960530
+    }
+  ]
 }
+
+module.exports = data
+
 
 module.exports = data
