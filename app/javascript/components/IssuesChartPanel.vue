f994c50f057bb22beb72ffaf44dce7840fb78f28
Robin Hawkes <rob.hawkes@gmail.com>
2018-07-17 12:43:21 +0100

    Move chart panel to sub-component

---
+++ app/javascript/components/IssuesChartPanel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="issues-chart__panel" :class="{active: active}">
+    <div class="issues-chart__panel-issues">
+      <span>{{values.issuesReported}}</span>
+      <p>Issues reported</p>
+    </div>
+
+    <div class="issues-chart__panel-countries">
+      <span>{{values.countriesReported}}</span>
+      <p>Countries reported</p>
+    </div>
+
+    <div class="issues-chart__panel-countries">
+      <span>{{values.countriesYetToReport}}</span>
+      <p>Countries yet to report</p>
+    </div>
+
+    <div class="issues-chart__label">{{values.year}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['values', 'active'],
+  data () {
+    return {}
+  }
+}
+</script>
