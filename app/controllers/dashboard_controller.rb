fedca515363a6ba97dc9fa37f141504bd9d30da3
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-07-18 16:03:00 +0100

    Authenticate user before actions

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @appendix_i_total_cnt = ShipmentsApiRetriever.api_call('appendix_i').first['meta']['total']
-    @trade_suspensions_total_cnt = ShipmentsApiRetriever.api_call('trade_suspensions').first['meta']['total']
-    @mandatory_quotas_total_cnt = ShipmentsApiRetriever.api_call('mandatory_quotas').first['meta']['total']
+    #@appendix_i_total_cnt = ShipmentsApiRetriever.api_call('appendix_i')['meta']['total']
+    #@trade_suspensions_total_cnt = ShipmentsApiRetriever.api_call('trade_suspensions')['meta']['total']
+    #@mandatory_quotas_total_cnt = ShipmentsApiRetriever.api_call('mandatory_quotas')['meta']['total']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   get 'download/download_all'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#index'
