ab3d24fa34d6e44f482a1cd4fe821bdcba3bead1
lucacug <luca.cugliari@unep-wcmc.org>
2018-07-23 11:10:22 +0100

    fix header and add parameter to filter data per year

--- lib/modules/shipments_api_retriever.rb
+++ lib/modules/shipments_api_retriever.rb
@@ -1,9 +1,9 @@
 module  ShipmentsApiRetriever
-  def self.api_call(compliance_type = nil, page = 1)
+  def self.api_call(compliance_type = nil, page = 1, year = nil)
     response = HTTParty.get(Rails.application.secrets['species_api_url'],
                             headers: header,
                             query: { compliance_type: compliance_type,
-                                     time_range_start: 2012, time_range_end: 2016,
+                                     time_range_start: year || 2012, time_range_end: year || 2016,
                                      page: page, per_page: 100_00 })
     JSON.parse(response.body)
   end
