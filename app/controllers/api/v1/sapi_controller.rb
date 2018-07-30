db48429e398cef40c04484e37eb037cac8fb5ea9
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-07-30 14:03:38 +0100

    Add authentication and page parameter

--- app/controllers/api/v1/sapi_controller.rb
+++ app/controllers/api/v1/sapi_controller.rb
@@ -1,14 +1,22 @@
 class Api::V1::SapiController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    call = "#{api_params[:call]}_call"
-    @data = ShipmentsApiRetriever.send(call, api_params[:type])
+    call = "#{sapi_params[:call]}_call"
+    @data = ShipmentsApiRetriever.send(call, sapi_params[:grouping])
 
     render json: @data.to_json
   end
 
   private
 
-  def api_params
-    params.permit(:call, :type)
+  def sapi_params
+    params.require(:sapi).permit(:call, :grouping, :user_id, :page)
+  end
+
+  def authenticate_user
+    unless sapi_params[:user_id].present? && sapi_params[:user_id] == current_user&.id
+      head(401)
+    end
   end
 end
