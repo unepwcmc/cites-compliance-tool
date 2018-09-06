43209e609ae5a5c5e57476c1ea1824720beb3139
lucacug <luca.cugliari@unep-wcmc.org>
2018-09-06 14:29:20 +0100

    update csv files header

--- lib/modules/csv_downloader.rb
+++ lib/modules/csv_downloader.rb
@@ -1,10 +1,8 @@
 module CsvDownloader
-  HEADER = %w[id year appendix taxon_name taxon_concept_id class_name order_name
-              family_name genus_name term term_id importer_reported_quantity
-              exporter_reported_quantity unit importer importer_iso importer_id
-              exporter exporter_iso exporter_id origin purpose source
-              import_permit export_permit origin_permit issue_type
-              rank_name].freeze
+  HEADER = %w[Shipment\ ID Year App. Taxon Class Order Family Genus Term
+              Importer\ reported\ quantity Exporter\ reported\ quantity Unit
+              Importer Exporter Origin Purpose Source Import\ Permit
+              Export\ Permit Origin\ Permit Issue\ Type].freeze
 
   def self.csv_generator(data)
     return unless data
