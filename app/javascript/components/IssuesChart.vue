91f5c3f129054cc4834f4f9424206d9207a492e0
Robin Hawkes <rob.hawkes@gmail.com>
2018-09-12 12:27:48 +0100

    Fix loading indicator for chart component

--- app/javascript/components/IssuesChart.vue
+++ app/javascript/components/IssuesChart.vue
@@ -2,37 +2,39 @@
   <section class="issues-chart tile__box">
     <component-loading v-if="!values"></component-loading>
 
-    <div v-show="values" class="issues-chart__svg">
-      <svg width="100%" height="100%" viewBox="0 0 400 100" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <linearGradient id="lineGradient" x1="0" x2="1" y1="0" y2="0">
-            <stop offset="10%" stop-color="#039ed5"/>
-            <stop offset="95%" stop-color="#b3c92a"/>
-          </linearGradient>
-        </defs>
+    <div v-if="values">
+      <div class="issues-chart__svg">
+        <svg width="100%" height="100%" viewBox="0 0 400 100" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient id="lineGradient" x1="0" x2="1" y1="0" y2="0">
+              <stop offset="10%" stop-color="#039ed5"/>
+              <stop offset="95%" stop-color="#b3c92a"/>
+            </linearGradient>
+          </defs>
 
-        <!--
-        Offset first path point ever so slightly so it gets a bounding box
-        http://www.w3.org/TR/SVG11/coords.html#ObjectBoundingBox
-        -->
-        <path :d="getPath()" fill="none" stroke="url(#lineGradient)" stroke-width="4"/>
+          <!--
+          Offset first path point ever so slightly so it gets a bounding box
+          http://www.w3.org/TR/SVG11/coords.html#ObjectBoundingBox
+          -->
+          <path :d="getPath()" fill="none" stroke="url(#lineGradient)" stroke-width="4"/>
 
-        <circle v-if="!isFirstYear()" cx="60" :cy="normaliseValue(getFirstValue().issuesReported)" r="4" fill="#4D6B89" stroke="#fff" stroke-width="2"/>
-        <circle cx="200" :cy="normaliseValue(getSecondValue().issuesReported)" r="4" fill="#4D6B89" stroke="#fff" stroke-width="2"/>
-        <circle v-if="!isLastYear()" cx="340" :cy="normaliseValue(getThirdValue().issuesReported)" r="4" fill="#4D6B89" stroke="#fff" stroke-width="2"/>
-      </svg>
-    </div>
+          <circle v-if="!isFirstYear()" cx="60" :cy="normaliseValue(getFirstValue().issuesReported)" r="4" fill="#4D6B89" stroke="#fff" stroke-width="2"/>
+          <circle cx="200" :cy="normaliseValue(getSecondValue().issuesReported)" r="4" fill="#4D6B89" stroke="#fff" stroke-width="2"/>
+          <circle v-if="!isLastYear()" cx="340" :cy="normaliseValue(getThirdValue().issuesReported)" r="4" fill="#4D6B89" stroke="#fff" stroke-width="2"/>
+        </svg>
+      </div>
 
-    <div class="issues-chart__panel-column is-first">
-      <issues-chart-panel v-if="getFirstValue()" :values="getFirstValue()"></issues-chart-panel>
-    </div>
+      <div class="issues-chart__panel-column is-first">
+        <issues-chart-panel v-if="getFirstValue()" :values="getFirstValue()"></issues-chart-panel>
+      </div>
 
-    <div class="issues-chart__panel-column is-second">
-      <issues-chart-panel :values="getSecondValue()" :active="true"></issues-chart-panel>
-    </div>
+      <div class="issues-chart__panel-column is-second">
+        <issues-chart-panel :values="getSecondValue()" :active="true"></issues-chart-panel>
+      </div>
 
-    <div class="issues-chart__panel-column is-third">
-      <issues-chart-panel v-if="getThirdValue()" :values="getThirdValue()"></issues-chart-panel>
+      <div class="issues-chart__panel-column is-third">
+        <issues-chart-panel v-if="getThirdValue()" :values="getThirdValue()"></issues-chart-panel>
+      </div>
     </div>
   </section>
 </template>
