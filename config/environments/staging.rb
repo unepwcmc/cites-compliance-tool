afda0abe859fcb959437351552456c28a7ba7c4a
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-02-28 15:46:36 +0000

    Add mailer host in secrets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://cites-compliance-tool.sapi-staging.linode.unep-wcmc.org' }
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.dig(:mailer, :host) }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :sendmail
 end
